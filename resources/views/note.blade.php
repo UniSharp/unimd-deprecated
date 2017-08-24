46b6f5ca107605da146aa8c36dbdae7db1bc52cf
Stream <stream@unisharp.com>
2017-08-24 15:21:10 +0800

    包裝 websocket 使用方式，並加上斷線自動重新連線功能

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -21,18 +21,18 @@
 <script src="https://codemirror.net/addon/merge/merge.js"></script>
 
 <script type="text/javascript">
-    var wsUri = "{{env('WS_URI') ?: 'ws://'.config('swoole.websocket.server').':'.config('swoole.websocket.port')}}"
-    var socket = new WebSocket(wsUri);
+    var wsUri = "{{env('WS_URI') ?: 'ws://'.config('swoole.websocket.server').':'.config('swoole.websocket.port')}}";
     var note_id = "{{$note->id}}";
+    var content = '';
 
     var editor = CodeMirror.fromTextArea(document.querySelector('#editor'), {
       lineNumbers: true,
       mode: 'markdown'
     })
     var dmp = new diff_match_patch();
-    var content = '';
     var diff = '';
-    var replaceRange = diff => editor.replaceRange(diff.text, diff.from, diff.to)
+    var replaceRange = diff => editor.replaceRange(diff.text, diff.from, diff.to);
+
     editor.on('change', function(editor, diff) {
         if (diff.origin && (diff.origin !== 'setValue')) {
             changeSend(diff);
@@ -42,55 +42,129 @@
         // console.log(diff);
     })
 
-    socket.onopen = function(ev) {
-        console.log('connected');
-        getNote(note_id);
-    }
-
-    socket.onclose = function(ev) {
-        console.log('closed');
-    }
-
-    socket.onerror = function(ev) {
-        console.log('error :' + ev.data);
-    }
-
-    socket.onmessage = function(ev) {
-        var data = JSON.parse(ev.data);
-        if (data.action === 'changeNote') {
-            replaceRange(data.message);
-        } else if (data.action === 'getNote' && data.message !== null) {
-            editor.getDoc().setValue(data.message);
-        } else if (data.action === 'getDiff') {
-            var patch = dmp.patch_fromText(data.message);
-            var apply = dmp.patch_apply(patch, editor.getDoc().getValue());
-            editor.getDoc().setValue(apply[0])
+    class Timer {
+        constructor (seconds) {
+            this.seconds = seconds;
+            this.countDownCallback = () => {};
+            this.completeCallback = () => {};
         }
-        console.log('message: ' + ev.data);
+
+        countDown (callback) {
+            this.countDownCallback = callback;
+            return this;
+        }
+
+        complete (callback) {
+            this.completeCallback = callback;
+            return this;
+        }
+
+        start () {
+            var seconds = this.seconds;
+            var that = this;
+
+            that.countDownCallback(seconds);
+            seconds--;
+
+            var counter = setInterval(function () {
+                if (seconds > 0) {
+                    that.countDownCallback(seconds);
+                    seconds--;
+                }
+            }, 1000);
+
+            setTimeout(function () {
+                clearInterval(counter);
+                that.completeCallback();
+            }, 1000 * (seconds + 1));
+        }
     }
+
+    class UniSocket {
+        constructor (wsUri) {
+            this.events = {};
+
+            this.wsUri = wsUri;
+
+            this.connect();
+        }
+
+        connect () {
+            this.socket = new WebSocket(this.wsUri);
+            this.socket.onopen = function(ev) {
+                console.log('connected');
+                getNote(note_id);
+            }
+
+            var that = this;
+
+            this.socket.onclose = function(ev) {
+                console.log('Connection lost.');
+
+                (new Timer(5))
+                    .countDown((seconds) => console.log('Retry connection in : ' + seconds + ' seconds'))
+                    .complete(() => {
+                        that.connect();
+                        console.log('restarting');
+                    })
+                    .start()
+            }
+
+            this.socket.onerror = function(ev) {
+                console.log(ev);
+            }
+        }
+
+        emit (event_name, data) {
+            data['action'] = event_name;
+            this.socket.send(JSON.stringify(data));
+            console.log('Event emited : ' + event_name);
+        }
+
+        on (event_name, callback) {
+            this.events[event_name] = callback;
+
+            var events = this.events;
+
+            this.socket.onmessage = function(ev) {
+                var data = JSON.parse(ev.data);
+                if (events[data.action] != undefined) {
+                    events[data.action](data);
+                }
+                console.log('message: ' + ev.data);
+            }
+            console.log('Event received : ' + event_name);
+        }
+    }
+
+    let io = new UniSocket(wsUri);
+
+    io.on('changeNote', (data) => {
+        replaceRange(data.message);
+    });
+
+    io.on('getNote', (data) => {
+        if (data.message !== null) {
+            editor.getDoc().setValue(data.message);
+        }
+    });
+
+    io.on('getDiff', (data) => {
+        var patch = dmp.patch_fromText(data.message);
+        var apply = dmp.patch_apply(patch, editor.getDoc().getValue());
+        editor.getDoc().setValue(apply[0]);
+    });
 
     function getNote(note_id) {
-        var msg = {
-            action: 'getNote',
-            note_id: note_id
-        };
-        socket.send(JSON.stringify(msg));
+        io.emit('getNote', { note_id: note_id });
     }
 
     function changeSend(diff) {
-        var msg = {
-            action: 'changeNote',
-            message: diff
-        };
-        socket.send(JSON.stringify(msg));
+        io.emit('changeNote', { message: diff });
     }
 
     function diffSend(diff) {
-        var msg = {
-            action: 'diffNote',
-            message: diff
-        };
-        socket.send(JSON.stringify(msg));
+        io.emit('diffNote', { message: diff });
     }
 </script>
 @endsection
